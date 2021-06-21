632a253dfb0869cf1021fa2f1cc4ac42df2a4759
Pasquale Perfetto <pasqualeperfetto@Pasquales-MacBook-Pro.local>
2021-06-21 12:25:55 +0200

    Environment setup

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
+    context: path.resolve(__dirname, 'src'),
+
+    entry: './js/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'assets'),
+        filename: './js/bundle.js'
+    },
+
+    module: {    
+        rules: [  
+            {  
+                test: /\.js$/i,  
+                include: path.resolve(__dirname, 'src'), 
+                use: {          
+                    loader: 'babel-loader', 
+                    options: {  
+                        presets: ['@babel/preset-env'],
+                    }, 
+                }, 
+            },
+            { 
+                test: /\.css$/i,
+                include: path.resolve(__dirname, 'src'), 
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            }, 
+        ], 
+    },
+
+}
