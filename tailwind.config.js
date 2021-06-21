758292cdaef9ad24d079767f3dcd0e389b748795
Pasquale Perfetto <pasqualeperfetto@Pasquales-MacBook-Pro.local>
2021-06-21 15:04:50 +0200

    Colors and fonts fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'consultant-red': '#fd5540',
-        'consultant-red-dark': '#e64d3a',
-        'consultant-light-blue': '#00c3d0',
-        'consultant-light-blue-dark': '#008891',
-        'consultant-light-blue-bold': '#00b1bd',
-        'consultant-yellow': '#f5c150',
-        'consultant-yellow-dark': '#dfaf49',
-        'consultant-gray': '#434a54',
-        'consultant-gray-dark': '#353a41',
-        'consultant-light-gray': '#b5babf',
-        'consultant-light-gray-dak': '#9aa0ab',
-        'consultant-bg': '#f8f8f2',
         'consultant-puple-light': '#9b9bca',
         'consultant-purple': '#696A86',
         'consultant-black-green': '#626780',
@@ -23,8 +11,8 @@ module.exports = {
       }
     },
     fontFamily: {
-      'display' : 'Signika',
-      'body': 'Montserrat',
+      'display' : 'DM Serif Display',
+      'body': 'Poppins',
      },
   },
   variants: {
