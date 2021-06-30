f9849042da0898d287092d506fefd6f7f904f42f
Pasquale Perfetto <pasqualeperfetto@Pasquales-MacBook-Pro.local>
2021-06-30 16:06:47 +0200

    reviews section complete

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
 import '../css/style.css';
 import Alpine from 'alpinejs';
+import Splide  from '@splidejs/splide';
+import faker from 'faker';
 window.Alpine = Alpine;
 
 Alpine.start();
 
+new Splide('#splide', {
+	type: 'loop',
+	pagination: false,
+    clones: 0,
+}).mount();
+
+let sentences = document.querySelectorAll('.review .sentence');
+
+sentences.forEach( (e) => {
+    e.innerText = faker.lorem.sentence()
+} )
+
+let avatars = document.querySelectorAll('.review .avatar');
+
+avatars.forEach( (e) => {
+    e.src = faker.image.avatar();
+} )
+
+let authors = document.querySelectorAll('.review .author-review');
+
+authors.forEach( (e) => {
+    e.innerText = `${faker.name.firstName()} ${faker.name.lastName()}`;
+} )
