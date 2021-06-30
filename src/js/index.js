180798b5669faa1f3353c6b2d50c7b48a498fa70
Pasquale Perfetto <pasqualeperfetto@Pasquales-MacBook-Pro.local>
2021-06-30 17:48:06 +0200

    form section complete

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,8 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
+// Slide reviews with dummy data
+
 new Splide('#splide', {
 	type: 'loop',
 	pagination: false,
@@ -29,3 +31,38 @@ let authors = document.querySelectorAll('.review .author-review');
 authors.forEach( (e) => {
     e.innerText = `${faker.name.firstName()} ${faker.name.lastName()}`;
 } )
+
+// end slide reviews with dummy data
+
+
+// Validation Sign In form
+
+const signForm = document.forms.signForm;
+const nameError = document.querySelector('#nameError');
+const pwdError = document.querySelector('#pwdError');
+
+signForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    let nameInput = signForm.elements.signName.value;
+
+    let checkNumbers = /\d/.test(nameInput);
+
+    let pwdInput = signForm.elements.signPassword.value;
+
+    if (nameInput.length < 3 || checkNumbers) {
+        nameError.innerText = 'Insert a valid name';
+    }
+    else if (pwdInput.length < 8) {
+        pwdError.innerText = 'Insert a valid password';
+    }
+    else {
+        alert('Registration successfully!');
+    }
+
+});
+
+
+
+
